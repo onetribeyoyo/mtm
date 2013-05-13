67a9a6f11efd1b4a6252f94bc78127376f413ff4
Andy Miller <amiller@objectpartners.com>
2013-05-13 10:51:35 -0500

    using alt paths so it doens't stomp on other projects in flight.

--- src/init.sh
+++ src/init.sh
@@ -2,7 +2,14 @@
 # mtm specific init
 #
 
-use jdk 1.6 grails 2.2.2 groovy 2.0.6
+use jdk -a 1.6
+rehome jdk alternate
+
+use grails -a 2.2.2
+rehome grails alternate
+
+use groovy -a 2.0.6
+rehome groovy alternate
 
 use # to echo the settings
 

f852772ef36707225cac717e9dee9f19ab93c387
Andy Miller <amiller@objectpartners.com>
2013-10-03 09:58:21 -0500

    translated junit test to spock test.

---
+++ src/test/unit/com/onetribeyoyo/mtm/services/StoryImportServiceSpec.groovy
@@ -0,0 +1,35 @@
+package com.onetribeyoyo.mtm.services
+
+import spock.lang.Specification
+
+class StoryImportServiceSpec extends Specification {
+
+    def "test extractField"() {
+        when:
+            def storyImportService = new StoryImportService()
+
+        then:
+            !storyImportService.extractString( [], 1 )
+            "a" == storyImportService.extractString( ["a", "b", "c"], 0 )
+            "b" == storyImportService.extractString( ["a", "b", "c"], 1 )
+            !storyImportService.extractString( ["a", "b", "c"], 4 )
+        and:
+            !storyImportService.extractBoolean( [], 0 )
+            !storyImportService.extractBoolean( [""], 0 )
+            !storyImportService.extractBoolean( ["    "], 0 )
+            !storyImportService.extractBoolean( ["false"], 0 )
+            !storyImportService.extractBoolean( ["False"], 0 )
+            !storyImportService.extractBoolean( ["FaLse"], 0 )
+            !storyImportService.extractBoolean( ["FALSE"], 0 )
+            storyImportService.extractBoolean( ["True"], 0 )
+            storyImportService.extractBoolean( ["non-false is true!"], 0 )
+        and:
+            storyImportService.extractLong( [], 0 ) == null
+            storyImportService.extractLong( [""], 0 ) == null
+            storyImportService.extractLong( [" "], 0 ) == null
+            storyImportService.extractLong( ["-1"], 0 ) == -1
+            storyImportService.extractLong( ["0"], 0 ) == 0
+            storyImportService.extractLong( ["42"], 0 ) == 42
+    }
+
+}

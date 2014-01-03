7d15b6d5111f046fb779e96076cea7124a46c9eb
Andy Miller <amiller@objectpartners.com>
2014-01-03 09:30:08 -0600

    refactored a bit of redundant code in the simplemodal taglib.

--- src/grails-app/controllers/com/onetribeyoyo/simplemodal/controllers/SimpleModalController.groovy
+++ src/grails-app/controllers/com/onetribeyoyo/simplemodal/controllers/SimpleModalController.groovy
@@ -1,15 +1,11 @@
 package com.onetribeyoyo.simplemodal.controllers
 
-//import grails.plugins.springsecurity.Secured
-
 class SimpleModalController {
 
-    //@Secured(['ROLE_USER'])
     def confirm = {
         render template: "confirm", model: [ : ]
     }
 
-    //@Secured(['ROLE_USER'])
     def confirmRemote = {
         render template: "confirmRemote", model: [ : ]
     }
